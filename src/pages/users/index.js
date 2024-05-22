470030c678e9b0917745f1a5aafcc3b57f0b1464
michael <michaelhernandezrala@gmail.com>
2024-05-22 16:17:38 +0200

    fix: fix user list view

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,9 +1,20 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Divider, List, TablePagination, TextField } from "@mui/material";
+import {
+  Box,
+  Divider,
+  FormControl,
+  InputLabel,
+  List,
+  MenuItem,
+  Select,
+  TablePagination,
+  TextField,
+} from "@mui/material";
 
 import UserService from "../../services/userService";
 import User from "./user";
 import Filters from "../../constants/filters";
+import MainLayout from "../../components/mainlayout";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -11,18 +22,21 @@ const Users = () => {
   const [page, setPage] = useState(Filters.PAGE);
   const [pageCount, setPageCount] = useState(0);
   const [find, setFind] = useState("");
+  const [sort, setSort] = useState("a-z");
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const query = find ? `?find=${find}` : `?limit=${limit}&page=${page}`;
-
+      let query = `?limit=${limit}&page=${page}&orderBy=${sort}`;
+      if (find) {
+        query += `&find=${find}`;
+      }
       const users = await UserService.findAll(query);
       setUsers(users.data);
       setPageCount(users.count);
     };
 
     fetchUsers();
-  }, [limit, page, find]);
+  }, [find, limit, page, sort]);
 
   const handleChangePage = (event, newPage) => {
     event.preventDefault();
@@ -39,15 +53,43 @@ const Users = () => {
     setFind(event.target.value);
   };
 
+  const handleSortChange = (event) => {
+    setSort(event.target.value);
+  };
+
   return (
-    <Fragment>
-      <TextField variant="outlined" value={find} onChange={handleSearchFind} />
-      <List
-        style={{
+    <MainLayout>
+      <Box
+        sx={{
           display: "flex",
-          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 4,
         }}
       >
+        <TextField
+          variant="standard"
+          value={find}
+          onChange={handleSearchFind}
+          size="normal"
+          placeholder="Buscar..."
+          label="Buscar"
+        />
+        <FormControl variant="standard" sx={{ width: "200px" }}>
+          <InputLabel id="order-by">Ordenar por</InputLabel>
+          <Select
+            labelId="order-by"
+            id="order-by"
+            label="Order by"
+            value={sort}
+            onChange={handleSortChange}
+          >
+            <MenuItem value="a-z">A-Z</MenuItem>
+            <MenuItem value="z-a">Z-A</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+      <List>
         {users.map((user) => (
           <Fragment key={user.id}>
             <User user={user} />
@@ -63,7 +105,7 @@ const Users = () => {
         rowsPerPage={limit}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
-    </Fragment>
+    </MainLayout>
   );
 };
 
--- src/pages/users/user/index.js
+++ src/pages/users/user/index.js
@@ -11,7 +11,16 @@ import {
 const User = ({ user }) => {
   return (
     <Link to={`/user/${user.id}`} style={{ textDecoration: "none" }}>
-      <ListItem key={user.id}>
+      <ListItem
+        key={user.id}
+        sx={{
+          border: "1px solid transparent",
+          transition: "border-color 0.3s",
+          "&:hover": {
+            borderColor: "primary.main",
+          },
+        }}
+      >
         <ListItemAvatar>
           <Avatar alt={user.name} src={user.logo} />
         </ListItemAvatar>
